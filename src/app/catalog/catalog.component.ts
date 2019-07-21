dc5d86b99d76e7d1eebdad4881b78a5b2a06b7a4
LesnikovD <bldfrstr@gmail.com>
2019-07-22 00:26:28 +0300

    Добавил список товаров

--- src/app/catalog/catalog.component.ts
+++ src/app/catalog/catalog.component.ts
@@ -1,15 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { Products } from '../products';
 
 @Component({
   selector: 'app-catalog',
   templateUrl: './catalog.component.html',
   styleUrls: ['./catalog.component.css']
 })
-export class CatalogComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+export class CatalogComponent {
+  products = Products;
 }
---
+++ src/app/products.ts
@@ -0,0 +1,17 @@
+export const Products = [
+  {
+    name: 'iPhone 5s',
+    img: 'assets/images/5s.jpeg',
+    price: 49000
+  },
+  {
+    name: 'iPhone 6',
+    img: 'assets/images/6.jpeg',
+    price: 59000
+  },
+  {
+    name: 'iPhone 6s',
+    img: 'assets/images/6s.jpeg',
+    price: 69000
+  }
+  ];
